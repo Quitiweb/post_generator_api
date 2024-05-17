8d411afb20b6b48ad3ca14c5039c8baebd1f81aa
Rafael <quitiweb@gmail.com>
2024-05-17 13:50:05 +0200

    Update post/gpt.py

--- post/gpt.py
+++ post/gpt.py
@@ -13,7 +13,6 @@ from .tools.utils import remove_html_tags
 
 load_dotenv()
 openai.api_key = config("OPENAI_API_KEY")
-#GPT_MODEL = "gpt-3.5-turbo"
 # GPT_MODEL = "gpt-3.5-turbo"
 GPT_MODEL = "gpt-4o"
 # GPT_MODEL = "gpt-4-32k"
