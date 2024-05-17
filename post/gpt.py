7e1fdb96c3905f08a59d33a85fa58eddfe278786
unknown <aruizbalaguer@gmail.com>
2024-05-17 13:25:42 +0200

    ya esta todo bien

--- post/gpt.py
+++ post/gpt.py
@@ -13,13 +13,9 @@ from .tools.utils import remove_html_tags
 
 load_dotenv()
 openai.api_key = config("OPENAI_API_KEY")
-<<<<<<< HEAD
 #GPT_MODEL = "gpt-3.5-turbo"
-GPT_MODEL = "gpt-4o"
-=======
 # GPT_MODEL = "gpt-3.5-turbo"
-GPT_MODEL = "gpt-4"
->>>>>>> eda8b8230aa5a11e249dc7079a28b914de4daf1e
+GPT_MODEL = "gpt-4o"
 # GPT_MODEL = "gpt-4-32k"
 # GPT_MODEL = "gpt-4-1106-preview"
 
