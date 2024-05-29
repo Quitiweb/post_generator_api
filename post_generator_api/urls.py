ecc33acec6ea9c547a4ba76c43ea7769994af8e8
unknown <aruizbalaguer@gmail.com>
2024-05-29 10:41:39 +0200

    he creado un archivo vista en el proyecto para que cuando salga el enlace pordefecto sin /admin ni nada, salga holaaaa

--- post_generator_api/urls.py
+++ post_generator_api/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from . import  views
 
 urlpatterns = [
+    path("", views.home),
     path('admin/', admin.site.urls),
     path("post/", include("post.urls", namespace="post"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
