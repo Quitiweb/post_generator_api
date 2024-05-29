2a0987edd2f55a9a67d7d1f9cf8e86dae5be33e4
unknown <aruizbalaguer@gmail.com>
2024-05-29 15:28:31 +0200

    He cambiado la hapariencia de la pagina home, de una frase a algo as bonito

--- post_generator_api/views.py
+++ post_generator_api/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
-
+from django.template import loader
 
 def home(request):
+    context = {'segment': 'index'}
 
-    return HttpResponse('Muy buenas ahora mismo se encuentra en la pagina home si desea hacer algo con la API porfavor ponga /admin seguido de la url que tiene ahora mismo')
+    html_template = loader.get_template('home/index.html')
+    return HttpResponse(html_template.render(context, request))
