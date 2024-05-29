27701a7923ebfa579ba6eb56e775eae45675b420
unknown <aruizbalaguer@gmail.com>
2024-05-29 11:24:53 +0200

    he cambiado el texto de holaaaaa a otra cosa

--- post_generator_api/views.py
+++ post_generator_api/views.py
@@ -5,4 +5,4 @@ from django.template import Template, Context
 
 def home(request):
 
-    return HttpResponse('holaaaaaaaaa')
+    return HttpResponse('Muy buenas ahora mismo se encuentra en la pagina home si desea hacer algo con la API porfavor ponga /admin seguido de la url que tiene ahora mismo')
